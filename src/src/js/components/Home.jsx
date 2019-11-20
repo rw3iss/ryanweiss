2e873c16e4ee47a21fb5eb98a1bc9424d1372f4c
Ryan Weiss <rw3iss@gmail.com>
2019-11-20 14:12:00 +0200

    Latest, added onenight project, small overall tweaks

--- src/src/js/components/Home.jsx
+++ src/src/js/components/Home.jsx
@@ -35,15 +35,15 @@ var Home = React.createClass({
                             <h3>I'm</h3>
                             <ul>
                                 <li>Ryan Weiss</li>
-                                <li>Web Developer of 15+ years</li>
-                                <li>Musician of 20+ years</li>
-                                <li>32yrs of age, 150lbs, 5'10"</li>
+                                <li>Web Developer: 15+ years</li>
+                                <li>Musician: 20+ years</li>
+                                <li>33yrs of age, 150lbs, 5'10"</li>
                             </ul>
 
                             <h3>Links</h3>
                             <ul>
-                                <li><Link to="/play">Demos</Link></li>
                                 <li><a target="_blank" href="/files/resume.pdf">Resume</a></li>
+                                <li><Link to="/play">Demos</Link></li>
                                 <li><a target="_blank" href="http://github.com/rw3iss">Github</a></li>
                                 <li><a href="mailto:rw3iss@gmail.com">rw3iss@gmail.com</a></li>
                             </ul>
@@ -53,21 +53,21 @@ var Home = React.createClass({
                         <div is class="section" id="skills">
                             <h3>Skills</h3>
                             <div is class="skill-types">  
-                                <h4>Frontend</h4>
+                                <h4>Frontend (<a target="_blank" href="/files/resume.pdf">full list</a>)</h4>
                                 <ul>
-                                    <li>JavaScript, React.js, Angular.js, Vue.js, Backbone.js, Mithril.js, Marko, ember.js, Knockout.js, HTML5, CSS3, SASS, CSS Flexbox and Grid, Canvas, WebGL, websockets, PWA's and service workers, jQuery, Backbone.js, Bootstrap, CoffeeScript, Handlebars, OAuth, Webpack, gulp, grunt, <a target="_blank" href="/files/resume.pdf">more</a></li>
+                                    <li>JavaScript, React.js/Preact, Angular.js, Vue.js, Backbone.js, Mithril.js, Marko, ember.js, Knockout.js, HTML5, CSS3, SASS, CSS Flexbox and Grid, Canvas, WebGL, websockets, PWA's and service workers, jQuery, Backbone.js, Bootstrap, CoffeeScript, Handlebars, OAuth, Webpack, gulp, grunt, <a target="_blank" href="/files/resume.pdf">more</a></li>
                                 </ul>
 
-                                <h4>Backend</h4>
+                                <h4>Backend (<a target="_blank" href="/files/resume.pdf">full list</a>)</h4>
                                 <ul>
-                                    <li>node.js, Express.js, PHP, ASP.NET, C#, Python, Java, Kotlin, Laravel, CodeIgniter, ExpressionEngine, Drupal, Wordpress, Directus, MySQL, PostgreSQL, websockets, MongoDB, e-commerce, Git, SSL, nginx, Apache, redis, OAuth, AWS, MVC, CQRS, message queues, continuous integration, unit testing, <a target="_blank" href="/files/resume.pdf">more</a></li>
+                                    <li>node.js, Express.js, PHP, ASP.NET, C#, Python, Java, Kotlin, Laravel, CodeIgniter, DialogFlow, ExpressionEngine, Drupal, Wordpress, Directus, MySQL, PostgreSQL, websockets, MongoDB, e-commerce, Git, SSL, nginx, Apache, redis, OAuth, AWS, MVC, CQRS, message queues, continuous integration, unit testing, <a target="_blank" href="/files/resume.pdf">more</a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div is class="line-title" id="fulltime"><h3>Latest</h3></div>
+                <div is class="line-title" id="fulltime"><h3>Latest</h3><h4>(updated November 2019)</h4></div>
                 <div is class="items" flex="row wrap">
                  {this.state.items.map(function(item, i){
                     return <Item item={item} key={i} />
